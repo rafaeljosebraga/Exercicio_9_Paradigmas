d5a8316ede2a7f7ffbc78e38caed04e7c08c9b45
Sarah Lino <156951487+SarahEmanuelleAlvesLino@users.noreply.github.com>
2025-06-13 20:18:15 -0300

    Adição da função remoção Duplicata
    Foi adicionado ao código main a função removeDuplicata( ) e os seu tente

--- main.go
+++ main.go
@@ -8,8 +8,28 @@ func executar_estrategia(lista *[]int, estrategia listFunction) {
 	*lista = estrategia(*lista)
 }
 
+func removeDuplicates(array []int) []int {
+	sorted := sortAscending(array)
+	cpr := array[0]
+	rem := make([]int, len(array))
+	rem[0] = cpr
+	k := 1
+
+	for i := 1; i < len(array); i++ {
+		if cpr != sorted[i] {
+			rem[k] = sorted[i]
+			cpr = sorted[i]
+			k++
+		}
+	}
+	return rem[0:k]
+}
+
 func main() {
 	primes := []int{11, 2, 2, 2, 5, 3, 3, 11, 5, 11, 13, 5, 2, 3, 11}
 	// executar_estrategia(&primes, removeDuplicates)
 	fmt.Println(primes)
+
+	listaSemDuplicatas := executar_estrategia(primes, removeDuplicates)
+	fmt.Println("Sem Duplicatas: ", listaSemDuplicatas)
 }
