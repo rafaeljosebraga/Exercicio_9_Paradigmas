4c4d0b262804320326fbe2ad732e504a79be143b
K3n1-Code <k3n1.work@gmail.com>
2025-06-14 09:26:21 -0300

    Ultima submição

--- main.go
+++ main.go
@@ -8,6 +8,37 @@ func executar_estrategia(lista *[]int, estrategia listFunction) {
 	*lista = estrategia(*lista)
 }
 
+func sortAscending(array []int) []int {
+	var swapped bool = true
+	var start int = 0
+	var end int = len(array) - 1
+
+	for swapped {
+		swapped = false
+
+		for i := start; i < end; i++ {
+			if array[i] > array[i+1] {
+				swap(&array[i], &array[i+1])
+				swapped = true
+			}
+		}
+		if !swapped {
+			break
+		}
+		swapped = false
+		end--
+
+		for i := end - 1; i >= start; i-- {
+			if array[i] > array[i+1] {
+				swap(&array[i], &array[i+1])
+				swapped = true
+			}
+		}
+		start++
+	}
+	return array
+}
+
 func main() {
 	primes := []int{11, 2, 2, 2, 5, 3, 3, 11, 5, 11, 13, 5, 2, 3, 11}
 	// executar_estrategia(&primes, removeDuplicates)
